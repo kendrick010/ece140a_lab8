f43a35e5a3e8199d57e8fe65dabf8a31d14f69fa
kendrick010 <nguyen.kendrick@yahoo.com>
2022-03-16 17:46:58 -0700

    test.py

--- test.py
+++ test.py
@@ -2,18 +2,47 @@ import numpy as np
 import cv2
 import os
 
+# Import all MySQL libraries
+import mysql.connector as mysql
+from dotenv import load_dotenv
+import os
+
+# Loads all details from the "credentials.env"
+load_dotenv('Challenge/credentials.env')
+db_host = os.environ['MYSQL_HOST']
+db_user = os.environ['MYSQL_USER']
+db_pass = os.environ['MYSQL_PASSWORD']
+db_name = os.environ['MYSQL_DATABASE']
+
 # Setting to camera capture to camera resolution 480 x 640
 cap=cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
+def hsv_parameters(object_name, frame):
+
+    # Convert to hsv deals better with lighting
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    hsv_ranges = 0
+
+    # Connect to database
+    db = mysql.connect(host=db_host, user=db_user, passwd=db_pass, database=db_name)
+    cursor = db.cursor()
+
+    # Get HSV range and polygon sides
+    cursor.execute("SELECT hue_lower, hue_upper, saturation_lower, saturation_upper, brightness_lower, brightness_upper FROM objects WHERE object_name = '" + object_name + "';")
+    result = cursor.fetchall()
+    db.close()
+    for i in result:
+        hsv_lower = np.array(i[0::2])
+        hsv_upper = np.array(i[1::2])
+        hsv_ranges += cv2.inRange(hsv, hsv_lower, hsv_upper)
+
+    return hsv_ranges
+
 try:
 	# Keep track of frames for debugging and image testing
 	frames = 0
-
-	if not os.path.exists("Tutorials/Frames/"):
-		os.mkdir("Tutorials/Frames/")
-		print("Created new directory for logging frames")
 
 	# Start live feed
 	while(frames >= 0):
@@ -25,27 +54,7 @@ try:
    		# Convert to hsv deals better with lighting
 		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-   		# Red is on the upper and lower of hsv scale. Requires 2 ranges
-		lower_red1 = np.array([0, 150, 20])
-		upper_red1 = np.array([10, 255, 255])
-		lower_red2 = np.array([160,100,20])
-		upper_red2 = np.array([179,255,255])
-		# Mask input image with upper and lower red ranges
-		red_only1 = cv2.inRange(hsv, lower_red1, upper_red1)
-		red_only2 = cv2.inRange(hsv, lower_red2 , upper_red2)
-		red_only = red_only1 + red_only2
-
-   		# Green is on the upper and lower of hsv scale. Requires 2 ranges
-		lower_green = np.array([40, 100, 20])
-		upper_green = np.array([75, 255, 255])
-		# Mask input image with upper and lower green ranges
-		green_only = cv2.inRange(hsv, lower_green , upper_green)
-
-   		# Blue is on the upper and lower of hsv scale. Requires 2 ranges
-		lower_blue = np.array([100, 100, 20])
-		upper_blue = np.array([125, 255, 255])
-		# Mask input image with upper and lower blue ranges
-		blue_only = cv2.inRange(hsv, lower_blue , upper_blue)
+		green_only = hsv_parameters("green_square", frame)
 		
 		# Mask for kernel opening
 		mask=np.ones((5,5),np.uint8)
@@ -78,8 +87,7 @@ try:
 
 			print("Green square found")
 
-			# Extracts the label of the largest none background component
-			# and displays distance from center and image.
+			# Extracts the label of the largest none background component and displays distance from center and image.
 			max_label, max_size = max([(i, stats[i, cv2.CC_STAT_AREA]) for i in range(1, num_labels)], key = lambda x: x[1])
 			
 			# Get only pixels with max_label as high (1), rest zero
@@ -91,10 +99,6 @@ try:
 			
 			# Get distance from center
 			#print('distance from center:', -1 * (320 - centroids[max_label][0]))
-
-			# Log images for debugging
-			#cv2.imwrite(f"Tutorials/Frames/data_{frames}.png", frame)
-			#cv2.imwrite(f"Tutorials/Frames/seg_{frames}.png", seg)
 
 			cv2.imshow("seg", seg)
 
